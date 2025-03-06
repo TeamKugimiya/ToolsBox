171d57459b7d1716a0e97a3986e06fe47a2222e1
Miku <26039249+xMikux@users.noreply.github.com>
2025-03-06 23:55:46 +0800

    簡化 codespace 載入時間

--- README.md
+++ README.md
@@ -4,16 +4,26 @@
 
 ### 如何使用？
 
-1. 安裝 Python 3.13 Up 環境（推薦使用 Codespace）
-2. 輸入指令
+1. 透過 CodeSpace 直接打開
+2. 在 [ParaTranz](https://paratranz.cn/users/my) 中的設定選項生成一個 API Token，這將是你的 `AUTH_TOKEN`（當使用完可以刪除掉）
+3. 點進你想要批量上傳的模組，進入編輯介面後會看到網址有 `?file=ID`，這就是 `FILE_ID` 的環境變數
+4. 上傳你的已翻譯好檔案，隨意命名都可以，檔案名稱將會是你的翻譯檔案路徑名稱 `TRANSLATED_FILE_PATH`
+5. 依序填入此設定，完成後開啟終端輸入
    ```bash
    export AUTH_TOKEN=
-   export PROJECT_ID=
+   export PROJECT_ID=9900
    export FILE_ID=
    export TRANSLATED_FILE_PATH=
    ```
-3. 輸入
+   範例：
    ```bash
-   poetry run bulk_update
+   export AUTH_TOKEN=abc
+   export PROJECT_ID=9900
+   export FILE_ID=1865207
+   export TRANSLATED_FILE_PATH=zh_tw.json
    ```
-4. 輸入後將會在終端上顯示結果
+6. 輸入完成環境變數後，在終端中打
+   ```bash
+   uv run bulk_update
+   ```
+7. 輸入後將會在終端上顯示結果
--- bulk_update/main.py
+++ bulk_update/main.py
@@ -3,7 +3,7 @@ import json
 from loguru import logger
 from paratranz_py import ParaTranz
 
-# Get ParaTranz String Data by File ID
+
 def get_string_id_dict(para: ParaTranz, project_id: int, file_id: int) -> dict:
     """
     透過 ParaTranz API 獲取特定檔案的詞條資訊
@@ -11,19 +11,22 @@ def get_string_id_dict(para: ParaTranz, project_id: int, file_id: int) -> dict:
     """
     retrun_data = {}
     data = para.strings.get_strings(
-        project_id=project_id,
-        file_id=file_id,
-        stage=None,
-        page_size=300)
+        project_id=project_id, file_id=file_id, stage=None, page_size=300
+    )
     for string_data in data["results"]:
         string_id = string_data["id"]
         string_key = string_data["key"]
         retrun_data[string_key] = string_id
-    logger.debug("Strings ID Key Dict: {}", retrun_data)
+    # logger.debug("Strings ID Key Dict: {}", retrun_data)
     return retrun_data
 
-# Bulk update strings
-def bulk_update_string(para: ParaTranz, project_id: int, strings_id_key_dict: dict, translated_strings: dict):
+
+def bulk_update_string(
+    para: ParaTranz,
+    project_id: int,
+    strings_id_key_dict: dict,
+    translated_strings: dict,
+):
     """
     透過 ParaTranz API 批量更新詞條資訊
     """
@@ -31,15 +34,21 @@ def bulk_update_string(para: ParaTranz, project_id: int, strings_id_key_dict: di
         if string_key in strings_id_key_dict:
             string_id = strings_id_key_dict[string_key]
             string_value = translated_strings[string_key]
-            logger.info("Updating string id:\n- Key: {} ({})\n- Translate: {}",string_key ,string_id, string_value)
+            logger.info(
+                "已更新磁條 ID:\n- 鍵值: {} ({})\n- 譯文: {}",
+                string_key,
+                string_id,
+                string_value,
+            )
             para.strings.update_string(
                 project_id=project_id,
                 string_id=string_id,
                 translate_text=string_value,
-                stage=1
+                stage=1,
             )
         else:
-            logger.warning("String key not found: {}", string_key)
+            logger.warning("詞條鍵值未找到: {}", string_key)
+
 
 def main():
     if not os.getenv("AUTH_TOKEN"):
@@ -57,6 +66,7 @@ def main():
     string_translated_dict = json.load(open(os.getenv("TRANSLATED_FILE_PATH")))
     strings_id_key_dict = get_string_id_dict(Para, project_id, file_id)
     bulk_update_string(Para, project_id, strings_id_key_dict, string_translated_dict)
+
 
 if __name__ == "__main__":
     main()
